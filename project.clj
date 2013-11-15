c8b5b3a579e6e39c65c0acc6e0378c61b84245c0
Justin May <justin.may@puppetlabs.com>
2013-11-14 16:29:23 -0800

    Fixed a couple things in your demo project file

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject puppetlabs/trapperkeeper "0.1.0-SNAPSHOT"
   :description "We are trapperkeeper.  We are one."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [puppetlabs/clj-utils "0.1.0-SNAPSHOT"]]
+                 [puppetlabs/kitchensink "0.1.0"]
+                 [prismatic/plumbing "0.1.0"]]
 
   :repositories [["releases" "http://nexus.delivery.puppetlabs.net/content/repositories/releases/"]
                  ["snapshots" "http://nexus.delivery.puppetlabs.net/content/repositories/snapshots/"]])

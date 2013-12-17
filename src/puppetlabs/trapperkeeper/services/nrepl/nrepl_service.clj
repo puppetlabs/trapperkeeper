6683f062f9702fba43db711c5c6627b1b6a2e9fa
Justin May <justin.may@puppetlabs.com>
2013-12-16 16:43:32 -0800

    PE-2319: Address review comments

--- src/puppetlabs/trapperkeeper/services/nrepl/nrepl_service.clj
+++ src/puppetlabs/trapperkeeper/services/nrepl/nrepl_service.clj
@@ -10,27 +10,23 @@
 (def ^{:private true} default-nrepl-port 7888)
 (def ^{:private true} default-bind-addr "0.0.0.0")
 
-(def ^{:private true} nrepl-server (atom nil))
-
 (defn- startup-nrepl
-  [config]
+  [config nrepl-server]
   {:pre [(map? config)]}
-  (let [enabled (parse-bool (get-in config [:nrepl :enabled] false))
-        port    (get-in config [:nrepl :port] default-nrepl-port)
-        bind    (get-in config [:nrepl :bind] default-bind-addr)]
-    (if (true? enabled)
+  (let [enabled?     (parse-bool (get-in config [:nrepl :enabled] false))
+        port         (get-in config [:nrepl :port] default-nrepl-port)
+        bind         (get-in config [:nrepl :bind] default-bind-addr)]
+    (if enabled?
       (do (log/debug "Starting nrepl service on port" port)
           (compare-and-set! nrepl-server nil (nrepl/start-server :port port :bind bind)))
-      (log/debug "nrepl service disabled, not starting")))
-
-  (println "nrepl server values: " @nrepl-server))
+      (log/debug "nrepl service disabled, not starting"))))
 
 (defn- shutdown-nrepl
-  [config]
+  [config nrepl-server]
   {:pre [(map? config)]}
-  (if (not (nil? @nrepl-server))
-    (do (log/debug "Shutting down nrepl service")
-        (nrepl/stop-server @nrepl-server))))
+  (when-not (nil? @nrepl-server)
+    (log/debug "Shutting down nrepl service")
+    (nrepl/stop-server @nrepl-server)))
 
 (defservice nrepl-service
   "The nREPL trapperkeeper service starts up a Clojure network REPL (nREPL) server attached to the running
@@ -44,8 +40,9 @@
    If no port is specified then the default port of 7888 is used."
   {:depends [[:config-service get-config]]
    :provides [shutdown]}
-  (startup-nrepl (get-config))
-  {:shutdown (partial shutdown-nrepl (get-config))})
+  (let [nrepl-server (atom nil)]
+    (startup-nrepl (get-config) nrepl-server)
+    {:shutdown (partial shutdown-nrepl (get-config) nrepl-server)}))
 
 
 

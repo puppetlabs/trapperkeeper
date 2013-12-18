929456c542c05e734cd767e7debd64c17ad97944
Justin May <justin.may@puppetlabs.com>
2013-12-17 16:18:10 -0800

    Fixed project file dups

--- project.clj
+++ project.clj
@@ -7,8 +7,6 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/tools.logging "0.2.6"]
                  [puppetlabs/kitchensink "0.3.1-SNAPSHOT"]
-                 [org.eclipse.jetty/jetty-server "7.6.1.v20120215"]
-                 [ring/ring-servlet "1.1.8"]
                  [prismatic/plumbing "0.1.0"]
                  [org.clojure/tools.nrepl "0.2.3"]
                  [log4j "1.2.17" :exclusions [javax.mail/mail

852c2b1ac60b4451fa265f0657f1b274bd65355e
Chris Price <chris@puppetlabs.com>
2014-01-08 10:38:29 -0800

    Bump version number to 0.1.1 for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/trapperkeeper "0.1.1-SNAPSHOT"
+(defproject puppetlabs/trapperkeeper "0.1.1"
   :description "We are trapperkeeper.  We are one."
   ;; Abort when version ranges or version conflicts are detected in
   ;; dependencies. Also supports :warn to simply emit warnings.

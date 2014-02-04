d24d91f99b8160179ee175544b6f7b613ab3b4fc
Chris Price <chris@puppetlabs.com>
2014-02-04 10:27:15 -0800

    Tweaks to support a jenkins release job

---
+++ jenkins/deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+git fetch --tags
+
+lein test
+echo "Tests passed!"
+
+lein release
+echo "Release plugin successful, pushing changes to git"
+
+git push origin
+git push origin --tags
+
+echo "git push successful."

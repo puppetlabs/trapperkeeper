c9ec9cfe39faf586d1af7a299ab2ac0399776efb
Jenkins User <jenkins@debian-7-x64.acctest.dc1.puppetlabs.net>
2014-03-22 06:30:25 -0700

    lein-release plugin: bumped version from 0.3.8 to 0.3.9-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def ks-version "0.5.3")
 
-(defproject puppetlabs/trapperkeeper "0.3.8"
+(defproject puppetlabs/trapperkeeper "0.3.9-SNAPSHOT"
   :description "We are trapperkeeper.  We are one."
   ;; Abort when version ranges or version conflicts are detected in
   ;; dependencies. Also supports :warn to simply emit warnings.

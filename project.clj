fc6ddbf56f6d38e0b5e374421d84cb22a26b0621
Jenkins CI <ci@puppetlabs.com>
2016-05-23 16:08:34 -0700

    Version 1.4.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def ks-version "1.3.0")
 (def logback-version "1.1.3")
 
-(defproject puppetlabs/trapperkeeper "1.4.1"
+(defproject puppetlabs/trapperkeeper "1.4.2-SNAPSHOT"
   :description "A framework for configuring, composing, and running Clojure services."
   ;; Abort when version ranges or version conflicts are detected in
   ;; dependencies. Also supports :warn to simply emit warnings.

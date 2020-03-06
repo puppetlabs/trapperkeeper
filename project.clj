458ab2ae4b15284f7adc5f7a61eb410c4df3e341
Rob Browning <rlb@puppet.com>
2020-03-06 11:44:56 -0600

    (PDB-4636) Version 3.1.0-SNAPSHOT
    Create new Y for upcoming ShutdownService request-shutdown changes.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/trapperkeeper "3.0.1-SNAPSHOT"
+(defproject puppetlabs/trapperkeeper "3.1.0-SNAPSHOT"
   :description "A framework for configuring, composing, and running Clojure services."
 
   :license {:name "Apache License, Version 2.0"

ed69b684a77ea4408bd5bb1ba29644734146ca0c
Jenkins CI <ci@puppet.com>
2023-03-14 00:05:14 +0000

    Version 3.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject puppetlabs/trapperkeeper "3.3.0"
+(defproject puppetlabs/trapperkeeper "3.3.1-SNAPSHOT"
   :description "A framework for configuring, composing, and running Clojure services."
 
   :license {:name "Apache License, Version 2.0"
